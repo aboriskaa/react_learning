5aad82dd2d3d4b7cac756c6899c17872c384cc31
aboriska <aboriska@yahoo.com>
2022-05-11 00:08:29 +0300

    L12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = () => {
+    return <header className='header'>
+        <img src='logo_plane.png' alt='logo' />
+    </header>
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cameralabs.org/media/k2/items/cache/e7b2aa61373306f3d6ce9012612d185c_L.jpg' alt='prof' width='100%' />
+        </div>
+        <div>
+            AVA + Description
+        </div>
+        <div>
+            My posts
+            <div>New post</div>
+        </div>
+        <div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    </div>
+}
+
+export default Profile;
+
