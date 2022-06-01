ac70a19188744582313772202ad7649b0a5e3321
aboriska <aboriska@yahoo.com>
2022-06-01 16:41:29 +0300

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import * as axios from "axios";
 import classes from './Users.module.css';
+import userPhoto from '../../assets/images/user_img.png'
 
 
 
 let Users = (props) => {
 
-    // if (props.users.length === 0) {
+    if (props.users.length === 0) {
 
-    //     props.setUsers(
-    //         [
-    //             { id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/4078437/pub_606a430a45570e1e4307d1f8_606a433cdae78e3f28ba4932/scale_1200', followed: false, fullName: 'Dmitry', status: 'I am a Boss', location: { city: 'Minsk', country: 'Belarus' } },
-    //             { id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1773286/pub_5d2b624c998ed600acf9b237_5d2b62fac49f2900aef41c3c/scale_1200', followed: true, fullName: 'Sasha', status: 'I am a Boss2', location: { city: 'Moscow', country: 'Russia' } },
-    //             { id: 3, photoUrl: 'https://s3.stc.all.kpcdn.net/putevoditel/serialy/wp-content/uploads/2021/12/Screenshot-2021-12-09-at-01-25-13-20001110_gaf_rs56_219-jpg-Izobrazhenie-JPEG-2000-3008-pikselov-Masshtabirovannoe-28.jpg', followed: false, fullName: 'Andry', status: 'I am a Boss3', location: { city: 'Kiev', country: 'Ukraine' } }
-    //         ]
-    //     );
-    // }
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        })
+
+        //     props.setUsers(
+        //         [
+        //             { id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/4078437/pub_606a430a45570e1e4307d1f8_606a433cdae78e3f28ba4932/scale_1200', followed: false, fullName: 'Dmitry', status: 'I am a Boss', location: { city: 'Minsk', country: 'Belarus' } },
+        //             { id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1773286/pub_5d2b624c998ed600acf9b237_5d2b62fac49f2900aef41c3c/scale_1200', followed: true, fullName: 'Sasha', status: 'I am a Boss2', location: { city: 'Moscow', country: 'Russia' } },
+        //             { id: 3, photoUrl: 'https://s3.stc.all.kpcdn.net/putevoditel/serialy/wp-content/uploads/2021/12/Screenshot-2021-12-09-at-01-25-13-20001110_gaf_rs56_219-jpg-Izobrazhenie-JPEG-2000-3008-pikselov-Masshtabirovannoe-28.jpg', followed: false, fullName: 'Andry', status: 'I am a Boss3', location: { city: 'Kiev', country: 'Ukraine' } }
+        //         ]
+        //     );
+    }
 
     return <div className={classes.usersBody}>
         {
@@ -22,7 +28,9 @@ let Users = (props) => {
 
                 <span>
                     <div>
-                        <img className={classes.photo} src={u.photoUrl} alt="AVA" />
+                        <img className={classes.photo} src={
+                            u.photos.small != null ? u.photos.small : userPhoto
+                        } alt="AVA" />
                     </div>
                     <div>
                         {
@@ -35,16 +43,17 @@ let Users = (props) => {
                     </div>
                 </span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </div>)
         }
     </div>
+
 }
 
 export default Users;
--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -3,11 +3,7 @@ const UN_FOLLOW = 'UN_FOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [
-        { id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/4078437/pub_606a430a45570e1e4307d1f8_606a433cdae78e3f28ba4932/scale_1200', followed: false, fullName: 'Dmitry', status: 'I am a Boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/1773286/pub_5d2b624c998ed600acf9b237_5d2b62fac49f2900aef41c3c/scale_1200', followed: true, fullName: 'Sasha', status: 'I am a Boss2', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, photoUrl: 'https://s3.stc.all.kpcdn.net/putevoditel/serialy/wp-content/uploads/2021/12/Screenshot-2021-12-09-at-01-25-13-20001110_gaf_rs56_219-jpg-Izobrazhenie-JPEG-2000-3008-pikselov-Masshtabirovannoe-28.jpg', followed: false, fullName: 'Andry', status: 'I am a Boss3', location: { city: 'Kiev', country: 'Ukraine' } }
-    ]
+    users: []
 }
 
 export const usersReducer = (state = initialState, action) => {
@@ -36,7 +32,10 @@ export const usersReducer = (state = initialState, action) => {
         case SET_USERS:
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                users: [
+                    // ...state.users,
+                    ...action.users
+                ]
 
             }
         default:
